7a42bf6758839bb899eb91983ea5feb281fdec02
Donggeon <goldeastfounder@gmail.com>
2025-07-31 15:57:44 +0900

    CORSMiddleware 추가

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from routes.rooms import router as rooms_router
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
 app.include_router(rooms_router, prefix="/api")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # Vue dev server 주소
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
