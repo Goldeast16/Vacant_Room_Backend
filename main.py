7f347cf1efe379af3fc0c05aef8d5f990843b968
Donggeon <goldeastfounder@gmail.com>
2025-08-01 14:45:52 +0900

    CORS edit

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ app.include_router(rooms_router, prefix="/api")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://vacantroom-kvlj3iqcv-kimdongs-projects-b6fa48d9.vercel.app"],  # Vue dev server 주소
+    allow_origins=[
+        "https://vacantroom-kvlj3iqcv-kimdongs-projects-b6fa48d9.vercel.app",
+        "https://vacantroom.vercel.app"
+    ],  # Vue dev server 주소
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
