a6d19d13b2d79500529a100110c7767a93896947
Donggeon <goldeastfounder@gmail.com>
2025-08-06 14:33:02 +0900

    GET/ health 추가

--- main.py
+++ main.py
@@ -13,23 +13,23 @@ MONGODB_URI = os.getenv("MONGODB_URI")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # ✅ 앱 시작 시 MongoDB 연결
+    # 앱 시작 시 MongoDB 연결
     mongo_client = AsyncIOMotorClient(MONGODB_URI)
     app.mongodb_client = mongo_client
     app.database = mongo_client.get_default_database()
 
     try:
         await app.database.command("ping")
-        print("✅ MongoDB 연결 성공")
+        print("MongoDB 연결 성공")
     except Exception as e:
-        print("❌ MongoDB 연결 실패:", e)
+        print("MongoDB 연결 실패:", e)
         raise e
 
     yield
 
-    # ✅ 앱 종료 시 연결 해제
+    # 앱 종료 시 연결 해제
     mongo_client.close()
-    print("🛑 MongoDB 연결 종료")
+    print("MongoDB 연결 종료")
 
 app = FastAPI(lifespan=lifespan)
 
